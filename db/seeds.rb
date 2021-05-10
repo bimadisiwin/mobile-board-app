1c7fe125dd6d22db1d45318ce9b10cedf5da7475
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2021-05-10 17:45:11 +0900

    掲示板一覧表示機能を実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  board = Board.create!(
+    name: Gimei.name,
+    title: Faker::Movie.title,
+    body: Faker::Lorem.sentence
+  )
+end
