271803fb5b771dc8fde8a9b7dd104808cc184751
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2021-05-12 09:22:42 +0900

    掲示板詳細表示機能を実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -12,11 +12,15 @@ class BoardsController < ApplicationController
     if board.save
       redirect_to boards_path, flash: { notice: "「 #{board.title} の掲示板を作成しました」" }
     else
-      redirect_to boards_new_path, flash: {
+      redirect_to new_board_path, flash: {
         board: board,
         error_messages: board.errors.full_messages
       }
     end
+  end
+
+  def show
+    @board = Board.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'boards', to: 'boards#index'
-  get 'boards/new', to: 'boards#new'
-  post 'boards', to: 'boards#create'
+  # get 'boards', to: 'boards#index'
+  # get 'boards/new', to: 'boards#new'
+  # post 'boards', to: 'boards#create'
+  resources :boards, only: %i[index new create show]
 end
