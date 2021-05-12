8985280952975767f36b76ee59c617ad5ec130bb
Yusuke Yonekura <42902626+bimadisiwin@users.noreply.github.com>
2021-05-12 10:42:07 +0900

    System Specを修正・追加

--- spec/system/boards_spec.rb
+++ spec/system/boards_spec.rb
@@ -1,18 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :system do
-  before do
-    @board = create(:board)
-  end
-
+RSpec.describe 'Boards', type: :system do
   context 'タイトルと本文が入力された場合' do
     it '掲示板の新規投稿が成功' do
-      visit boards_new_path(@board)
-
+      visit new_board_path
       expect(page).to have_field 'タイトル'
       fill_in 'タイトル', with: 'テストのタイトル'
       fill_in '本文', with: 'テストの本文'
-
       click_button '保存'
       expect(page).to have_content 'テストのタイトル の掲示板を作成しました'
       expect(page).to have_content 'テストのタイトル'
@@ -21,14 +15,35 @@ RSpec.describe 'Users', type: :system do
 
   context 'タイトルが入力されていない場合' do
     it '掲示板の新規投稿が失敗' do
-      visit boards_new_path(@board)
-
+      visit new_board_path
       expect(page).to have_field 'タイトル'
       fill_in 'タイトル', with: nil
       fill_in '本文', with: 'テストの本文'
-
       click_button '保存'
       expect(page).to have_content 'タイトルを入力してください'
+    end
+  end
+
+  context '掲示板一覧から画面遷移する場合' do
+    before do
+      create(:board)
+    end
+
+    it '新規作成画面に遷移できる' do
+      visit boards_path
+      expect(page).to have_content '掲示板'
+      expect(page).to have_link '新規作成'
+      click_on '新規作成'
+      expect(page).to have_content '新規追加'
+      click_on '一覧'
+    end
+
+    it '掲示板詳細画面に遷移できる' do
+      visit boards_path
+      expect(page).to have_content '掲示板'
+      expect(page).to have_link '詳細'
+      click_link '詳細'
+      expect(page).to have_content '掲示板詳細'
     end
   end
 end
